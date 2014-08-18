e1cd454407e4c4901cee762b185bee0fdd0821ef
Mario Alviano <mario@alviano.net>
2014-08-18 16:18:58 +0200

    Add disjunction and choice.

--- reverse_lparse/reverse_lparse.py
+++ reverse_lparse/reverse_lparse.py
@@ -22,14 +22,37 @@ def readProgram(line):
             pos.append(line[i])
             if line[i] not in names:
                 names[line[i]] = line[i]
-        rules.append((head, pos, neg))
+        rules.append((head, pos, neg, False))
+    elif line[0] == '8' or line[0] == '3':
+        headSize = int(line[1])
+        bodySize = int(line[headSize+2])
+        negativeSize = int(line[headSize+3])
+        head = []
+        for i in range(2,2+headSize):
+            head.append(line[i])
+            if line[i] not in names:
+                names[line[i]] = line[i]
+        pos = []
+        neg = []
+        for i in range(4+headSize,4+headSize+negativeSize):
+            neg.append(line[i])
+            if line[i] not in names:
+                names[line[i]] = line[i]
+        for i in range(4+headSize+negativeSize,len(line)):
+            pos.append(line[i])
+            if line[i] not in names:
+                names[line[i]] = line[i]
+        rules.append((head, pos, neg, line[0] == '3'))
     
 def readNames(line):
     names[line[0]] = line[1]
     
 def printProgram():
     for rule in rules:
-        print("%s%s%s%s%s%s%s." % (" | ".join([names[a] for a in rule[0]]), " " if rule[0] else "", ":- " if rule[1] or rule[2] else "", ", ".join([names[a] for a in rule[1]]), ", " if len(rule[1]) > 0 and len(rule[2]) > 0 else "", "not " if len(rule[2]) > 0 else "", ", not ".join([names[a] for a in rule[2]])))
+        if rule[-1]:
+            print("{%s}%s%s%s%s%s%s." % (", ".join([names[a] for a in rule[0]]), " " if rule[0] else "", ":- " if rule[1] or rule[2] else "", ", ".join([names[a] for a in rule[1]]), ", " if len(rule[1]) > 0 and len(rule[2]) > 0 else "", "not " if len(rule[2]) > 0 else "", ", not ".join([names[a] for a in rule[2]])))
+        else:
+            print("%s%s%s%s%s%s%s." % (" | ".join([names[a] for a in rule[0]]), " " if rule[0] else "", ":- " if rule[1] or rule[2] else "", ", ".join([names[a] for a in rule[1]]), ", " if len(rule[1]) > 0 and len(rule[2]) > 0 else "", "not " if len(rule[2]) > 0 else "", ", not ".join([names[a] for a in rule[2]])))
 
 callback = [readProgram, readNames]
 state = 0
