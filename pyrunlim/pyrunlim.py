804f09728a1e9afb58a548493452e2957833edda
Mario Alviano <mario@alviano.net>
2014-09-18 17:43:38 +0200

    Increase version.

--- pyrunlim/pyrunlim.py
+++ pyrunlim/pyrunlim.py
@@ -18,7 +18,7 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 """
 
-VERSION = "2.4"
+VERSION = "2.5"
 
 import argparse
 import psutil
